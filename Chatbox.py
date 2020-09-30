2e89e1c260bee0013322701c4fa26e65e0e0916a
Debarghya Das <72141607+MKBHD-alpha@users.noreply.github.com>
2020-09-30 21:58:53 +0530

    Chatbox V1

---
+++ Chatbox.py
@@ -0,0 +1,24 @@
+from tkinter import *
+root=Tk()
+
+def send():
+    send="you:"+a.get()
+    text.insert(END,"\n"+send)
+    if (a.get()=="hi"):
+        text.insert(END,"\n"+"Bot:hello")
+    elif(a.get()=="hello"):
+        text.insert(END, "\n" + "Bot:Bot:Hii")
+    elif(a.get()=="how are you"):
+        text.insert(END, "\n" + "Bot:I am Fine, How are you??")
+    elif(a.get()=="i am fine"):
+        text.insert(END, "\n" + "Bot:That's sound Good")
+    else:
+        text.insert(END, "\n" + "Bot:I didn't get it")
+
+text=Text(root,bg='Yellow')
+text.grid(row=10,column=0,columnspan=2)
+a=Entry(root,width=80)
+send=Button(root,text='Send',bg='Pink',width=20,command=send).grid(row=1,column=1)
+a.grid(row=1,column=0)
+root.title("Deepraj's Chatbot")
+root.mainloop()
